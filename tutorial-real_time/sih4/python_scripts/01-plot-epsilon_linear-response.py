55c16212b5c75cec61b9e5bbc0bad12d0dff1ff5
alexmoratalla <alejandro.molina@uv.es>
2019-09-11 11:11:51 +0100

    inputs files in SiH4

---
+++ tutorial-real_time/sih4/python_scripts/01-plot-epsilon_linear-response.py
@@ -0,0 +1,10 @@
+from numpy import loadtxt
+from pylab import *
+
+ref = loadtxt('refs/o-ip-ref.eps_q1_ip')
+
+xlabel('E (eV)')
+ylabel('$\chi$')
+plot(ref[:,0],ref[:,1],'o',label='$\chi$(reference)')
+legend()
+show()
